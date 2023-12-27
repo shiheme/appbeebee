300c7a8da6e73aa56ad74106ddc651bf84bf0469
魏福寿 <weifushou@MacBook-2023.local>
2023-12-27 21:17:02 +0800

    add logo

--- docs/.vitepress/theme/head.ts
+++ docs/.vitepress/theme/head.ts
@@ -1,5 +1,6 @@
 import type { HeadConfig } from 'vitepress';
-import { useData, withBase } from 'vitepress'
 export const head: HeadConfig[] = [
-  ['link', { rel: 'icon', href: '/vitepress/favicon.ico' }]
+  ['link', { rel: 'icon', href: '/favicon.ico' }],
+  ['link', { rel: 'icon', href: '/logo.png' }],
+  ['link', { rel: 'apple-touch-icon',sizes:'180x180', href: '/logo.png' }]
 ];
