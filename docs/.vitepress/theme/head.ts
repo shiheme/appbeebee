71f576f9dc9841239ce8542e50fe9c9e3d4d1d24
魏福寿 <weifushou@MacBook-2023.local>
2024-01-11 10:30:07 +0800

    add post

--- docs/.vitepress/theme/head.ts
+++ docs/.vitepress/theme/head.ts
@@ -2,5 +2,13 @@ import type { HeadConfig } from 'vitepress';
 export const head: HeadConfig[] = [
   ['link', { rel: 'icon', href: '/favicon.ico' }],
   ['link', { rel: 'icon', href: '/logo.png' }],
-  ['link', { rel: 'apple-touch-icon',sizes:'180x180', href: '/logo.png' }]
+  ['link', { rel: 'apple-touch-icon',sizes:'180x180', href: '/logo.png' }],
+  [
+    "script",
+    {
+      crossorigin: "anonymous", 
+      async: "",
+      src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1279518235550313",
+    },
+  ],
 ];
