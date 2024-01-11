b3e7eab3110bc1851e32b15a31013bbc4342dfab
魏福寿 <weifushou@MacBook-2023.local>
2024-01-11 11:26:18 +0800

    add post

--- docs/.vitepress/theme/head.ts
+++ docs/.vitepress/theme/head.ts
@@ -22,5 +22,18 @@ export const head: HeadConfig[] = [
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', 'G-QRQ5599P9G');` 
-  ]
+  ],
+  [
+    "script",
+    {
+      nonce: "X3-MeXfNjwsxlQdQcRqhtQ", 
+      async: "",
+      src: "https://fundingchoicesmessages.google.com/i/pub-1279518235550313?ers=1",
+    },
+  ],
+  [
+    'script',
+    {},
+    `(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();` 
+  ],
 ];
